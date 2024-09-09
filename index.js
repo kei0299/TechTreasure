f9a02b45b4e80ae21739fa8710e0a114436d5bf4
kei0299 <kitagen_1026@yahoo.co.jp>
2024-09-10 00:19:34 +0900

    deproy_test

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const port = 3000;
 const express = require("express");
+const path = require("path"); // パスの設定に使うモジュール
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("<h1>Hello Express.js!</h1>");
-}).listen(port, () => {
+// テンプレートエンジンとして EJS を使用する設定
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // views フォルダを設定
+
+// ルート設定
+app.get("/", (req, res) => {
+    res.render("index"); // "views" フォルダ内の "index.ejs" をレンダリング
+});
+
+// サーバーを指定ポートで起動
+app.listen(port, () => {
     console.log(`The server has started and is listening on port number: ${port}`);
 });
