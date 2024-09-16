0840689581f09817bff2235c4311b1c7afb15bca
kei0299 <kitagen_1026@yahoo.co.jp>
2024-09-17 02:05:18 +0900

    db→pool変数に変更

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const db = require('./db/db');
+const pool = require('./db/db');
 
 app.get("/", (req, res) => {
-    db.query(
+    pool.query(
         `select * from logo;`,(error, results) => {
           res.render('index', {
             title: 'TechTreasure',
